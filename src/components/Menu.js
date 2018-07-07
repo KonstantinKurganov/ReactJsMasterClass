abfab6646dde219701aa155e31cafbb5fa40790f
KonstantinKurganov <kurganovk@gmail.com>
2018-07-07 11:15:04 +0300

    added new files for new components

---
+++ src/components/Menu.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+import Grid from 'react-bootstrap/lib/Button';
+import Row from 'react-bootstrap/lib/Button';
+import Col from 'react-bootstrap/lib/Button';
+
+import styled from 'styled-components';
